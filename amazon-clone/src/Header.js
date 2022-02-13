1404a592547a217e18cb59143aeb55b6adbf8c13
Visels <elviskipchumba@yahoo.com>
2022-02-13 03:09:58 +0300

    Working on header section

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -3,7 +3,23 @@ import './Header.css'
 
 function Header() {
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+        
+        <img
+        className='header__logo'
+         src='http://pngimg.com/uploads/amazon_PNG11.png'
+         />
+
+    <div
+     className='header__search'>
+      <input className='header__search' type = "text"></input>
+
+
+    </div>
+           
+         
+        
+        </div>
   )
 }
 
