2668e6c1c24784cc8051bfad8f041be51033def7
Visels <elviskipchumba@yahoo.com>
2022-02-21 01:05:25 +0300

    product section complete

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,21 +1,46 @@
 import React from 'react'
 import './Home.css';
+import Product from './Product';
 
 
 function Home() {
   return (
     <div className='home'>
         
-<div className="home__container">
+    <div className="home__container">
     
     <img
     className='home__image' 
     src="https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3740_.jpg" alt="amazon_banner" />
     
+    <div className="home__row">
+        <Product />
+        <Product />
+        <Product />
+    {/* Product */}
+    {/* Product */}
+
+    </div>
+
+    <div className="home__row">
+    {/* Product */}
+    {/* Product */}
+    {/* Product */}
+
+
+    </div>
+    <div className="home__row">
+
+    {/* Product */}
+
+
+    </div>
+
+
     </div>        
         
         
-        </div>
+    </div>
   )
 }
 
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -5,8 +5,33 @@ function Product() {
   return (
     <div className='product'>
         
-        <p>I am a product</p>
-        
+    <div className="product__info">
+    
+    <p>Original HP 67XL Black High-yield Ink Cartridge |
+         Works with HP DeskJet 1255, 2700, 4100 Series, 
+         HP ENVY 6000, 6400 Series | Eligible for Instant
+          Ink | 3YM57AN</p>
+
+    <p className="product__price">
+        <small>$</small>
+        <strong>19.99</strong>
+    </p>
+
+    <div className="product__rating">
+
+        <p>⭐</p>
+        <p>⭐</p>
+        <p>⭐</p>
+        <p>⭐</p>
+
+    </div>
+    
+
+    <img src="https://m.media-amazon.com/images/I/61pUul1oDlL._AC_UL320_.jpg" alt="" />
+    
+    <button>Add to Basket</button>
+    
+    </div>        
         
         
         </div>
