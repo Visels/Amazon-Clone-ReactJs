10a9f371c2d1b3756a396277dd02b8bcb62150cf
Visels <elviskipchumba@yahoo.com>
2022-02-21 00:32:49 +0300

    Start of product section

---
+++ amazon-clone/src/Product.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Product.css'
+
+function Product() {
+  return (
+    <div className='product'>
+        
+        <p>I am a product</p>
+        
+        
+        
+        </div>
+  )
+}
+
+export default Product
