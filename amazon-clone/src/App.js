011b9215fca82898df4d1a2942ed16390ea02730
Visels <elviskipchumba@yahoo.com>
2022-02-22 12:22:49 +0300

    added react routers

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,21 +3,41 @@ import './App.css';
 import './Header';
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+
+    <Router>
+
     <div className="App">
-      
- 
 
-      {/* Header */}
-    <Header  />
 
-      {/* Home  */}
 
-      <Home />
+   <Switch>
+
+
+
+
+          <Route path='/checkout'>
+
+          <Header />
+          <h2>I am a checkout page</h2>
+
+          </Route>
     
+
+          <Route path='/'>
+
+          <Header />
+          <Home />
+
+          </Route>
+    
+   </Switch>
     </div>
+
+    </Router>
   );
 }
 
