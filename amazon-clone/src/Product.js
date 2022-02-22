c07748e518817e3b5f97389d6bd395de038dfe11
Visels <elviskipchumba@yahoo.com>
2022-02-22 04:07:49 +0300

    rating section complete

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,41 +1,45 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({title, image, price, rating}) {
   return (
     
     <div className="product">
 
-        
-    <div className="product__info">
-    
-    <p>Original HP 67XL Black High-yield Ink Cartridge |
-         Works with HP DeskJet 1255, 2700, 4100 Series, 
-         HP ENVY 6000, 6400 Series | Eligible for Instant
-          Ink | 3YM57AN</p>
+                  
+              <div className="product__info">
+              
+              <p>{title}</p>
 
-    <p className="product__price">
-        <small>$</small>
-        <strong>19.99</strong>
-    </p>
+              <p className="product__price">
+                  <small>$</small>
+                  <strong>{price}</strong>
+              </p>
 
-    <div className="product__rating">
+              <div className="product__rating">
 
-        <p>⭐</p>
-        <p>⭐</p>
-        <p>⭐</p>
-        <p>⭐</p>
 
-    </div>
-    
+              {Array(rating).fill().map((_,i) => {
 
-    <img src="https://m.media-amazon.com/images/I/61pUul1oDlL._AC_UL320_.jpg" alt="" />
-    
-    <button>Add to Basket</button>
-    
-    </div>        
-        
-        
+                return <p>⭐</p>
+
+              })}
+
+
+
+                  
+
+
+              </div>
+              
+
+              <img src={image} />
+              
+              <button>Add to Basket</button>
+              
+              </div>        
+                  
+                  
   
     </div>
 
