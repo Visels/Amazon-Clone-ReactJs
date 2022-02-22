1ab1e4d02eaf138fe59d852fc18da34861006439
Visels <elviskipchumba@yahoo.com>
2022-02-22 12:48:02 +0300

    checkout banner added

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -6,7 +6,36 @@ function Checkout() {
     <div className='checkout'>
 
 
-            <h1>This is the checkout page</h1>        
+                <div className="checkout__left">
+
+                <img
+                className='checkout__ad' 
+                src="https://images-na.ssl-images-amazon.com/images/I/7191qk-xnFL.jpg" alt="" />
+
+                <div >
+                    <h2 className="checkout__title">
+                        Your shopping basket
+                    </h2>
+
+                </div>
+
+
+
+                </div>
+
+
+
+                <div className="checkout__right">
+
+                    <h2>The subtotal will go here</h2>
+
+
+
+
+
+                </div>
+
+
         
         
         </div>
